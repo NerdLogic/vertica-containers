76630891275a89a3ed19fcc3967fd7be48d73eb8
Duane Bronson <bronson@nerdlogic.com>
2023-02-28 11:19:48 -0500

    WIP: use ssl in tests (example.sh)

--- vertica-kafka-scheduler/example.sh
+++ vertica-kafka-scheduler/example.sh
@@ -10,13 +10,14 @@ NETWORK=${COMPOSE_PROJECT_NAME}_example
 : ${VERTICA_VERSION:=v12.0.3}
 export VERTICA_K8S_VERSION=${VERTICA_VERSION#v}-0-minimal
 
+
 ########################
 # Debugging and Colors #
 ########################
 
 # TO only run certain steps, export steps variable like so:
-# steps="start setup run write" ./example.sh
-: ${steps=start setup run write stop clean}
+# steps="start setup" ./example.sh
+: ${steps=ssl start setup run write stop clean}
 
 # see if sed can make the log output green to make it easier to diferentiate
 esc=$'\e'
@@ -29,17 +30,73 @@ if ! echo | $green >/dev/null 2>&1; then
   blue=cat
 fi
 
+DOCKER_OPTS+=(
+  --rm \
+  -v $PWD/example.conf:/etc/vkconfig.conf \
+  -v $PWD/vkafka-log-config-debug.xml:/opt/vertica/packages/kafka/config/vkafka-log-config.xml \
+  -v $PWD/log:/opt/vertica/log \
+  --network $NETWORK \
+  --user $(id -u):$(id -g) \
+  --name kafka_scheduler \
+  )
+
+##########################
+# CREATE SSL CERTS       #
+##########################
+#
+# to turn off SSL, set NO_SSL to something like this:
+# NO_SSL=1 ./example.sh
+#
+if [[ -z $NO_SSL ]] ; then
+  PASSWORD=example
+  if [[ $steps =~ ssl ]]; then
+    rm -rf security
+    mkdir -p security
+
+    # create CA
+    OPENSSL="docker run --user $(id -u):$(id -g) -v $PWD:$PWD -w $PWD alpine/openssl"
+    $OPENSSL req -config openssl.cnf -new -x509 -keyout security/example.ca-key -out security/example.ca-cert -days 365 -subj "/C=US/ST=Massachusetts/L=Cambridge/CN=ca" -passout pass:$PASSWORD 2>&1 | $blue
+    docker run --user $(id -u):$(id -g) -v $PWD:$PWD -w $PWD alpine/openssl req -config openssl.cnf -new -x509 -keyout security/example.ca-key -out security/example.ca-cert -days 365 -subj "/C=US/ST=Massachusetts/L=Cambridge/CN=ca" -passout pass:$PASSWORD 2>&1 | $blue
+
+    # create truststore containing CA
+    KEYTOOL="docker run --user $(id -u):$(id -g) -v $PWD:$PWD -w $PWD vertica/kafka-scheduler:$VERTICA_VERSION keytool"
+    $KEYTOOL -keystore security/example.truststore.jks -deststoretype pkcs12 -alias caroot -import -file security/example.ca-cert -storepass $PASSWORD -keypass $PASSWORD -noprompt 2>&1 | $green
+
+    # kafka's keystore
+    $KEYTOOL -keystore security/exampleK.keystore.jks -deststoretype pkcs12 -alias exampleK -validity 365 -keyalg RSA -genkey -dname "CN=kafka,L=Cambridge,ST=Massachusetts,C=US" -storepass $PASSWORD -keypass $PASSWORD -noprompt 2>&1 | $blue
+    $KEYTOOL -keystore security/exampleK.keystore.jks-deststoretype pkcs12  -alias exampleK -certreq -file security/example.cert-file-K -storepass $PASSWORD -noprompt 2>&1 | $blue
+    $OPENSSL x509 -req -CA security/example.ca-cert -CAkey security/example.ca-key -in security/example.cert-file-K -out security/example.cert-signed-K -days 365 -CAcreateserial -passin pass:$PASSWORD 2>&1 | $blue
+    $KEYTOOL -keystore security/exampleK.keystore.jks -deststoretype pkcs12 -alias caroot -import -file security/example.ca-cert -storepass $PASSWORD -keypass $PASSWORD -noprompt 2>&1 | $blue
+    $KEYTOOL -keystore security/exampleK.keystore.jks -deststoretype pkcs12 -alias exampleK -import -file security/example.cert-signed-K -storepass $PASSWORD -keypass $PASSWORD -noprompt 2>&1 | $blue
+
+    # vertica's keystore
+    $KEYTOOL -keystore security/exampleV.keystore.jks -deststoretype pkcs12 -alias exampleV -validity 365 -keyalg RSA -genkey -dname "CN=vertica,L=Cambridge,ST=Massachusetts,C=US" -storepass $PASSWORD -keypass $PASSWORD -noprompt 2>&1 | $green
+    $KEYTOOL -keystore security/exampleV.keystore.jks -deststoretype pkcs12 -alias exampleV -certreq -file security/example.cert-file-V -storepass $PASSWORD -noprompt 2>&1 | $green
+    $OPENSSL x509 -req -CA security/example.ca-cert -CAkey security/example.ca-key -in security/example.cert-file-V -out security/example.cert-signed-V -days 365 -CAcreateserial -passin pass:$PASSWORD 2>&1 | $green
+    $KEYTOOL -keystore security/exampleV.keystore.jks -deststoretype pkcs12 -alias caroot -import -file security/example.ca-cert -storepass $PASSWORD -keypass $PASSWORD -noprompt 2>&1 | $green
+    $KEYTOOL -keystore security/exampleV.keystore.jks -deststoretype pkcs12 -alias exampleV -import -file security/example.cert-signed-V -storepass $PASSWORD -keypass $PASSWORD -noprompt 2>&1 | $green
+
+    DOCKER_OPTS+=( -v "$PWD/security/example0.keystore.jks:/etc/keystore.jks" )
+    DOCKER_OPTS+=( -v "$PWD/security/example.truststore.jks:/etc/truststore.jks" )
+    DOCKER_OPTS+=( -e VKCONFIG_JVM_OPTS="-Djavax.net.ssl.keyStore=/etc/keystore.jks -Djavax.net.ssl.keyStorepassword=$PASSWORD -Djavax.net.ssl.trustStore=/etc/truststore.jks" )
+
+  fi
+  DOCKER_COMPOSE_YML=docker-compose.yml
+else
+  DOCKER_COMPOSE_YML=docker-compose-nossl.yml
+fi
+
 ##########################
 # SETUP TEST ENVIRONMENT #
 ##########################
 if [[ $steps =~ start ]]; then
 
 # make sure containers have been cleaned up properly
-docker-compose rm -svf >/dev/null 2>&1 || exit $?
+docker-compose -f $DOCKER_COMPOSE_YML rm -svf >/dev/null 2>&1 || exit $?
 
 # start servers
 # docker-compose uses colors, so don't override
-docker-compose up -d --force-recreate
+docker-compose -f $DOCKER_COMPOSE_YML up -d --force-recreate
 
 # create a directory for log output
 mkdir -p log
@@ -51,22 +108,29 @@ if [[ $MACHTYPE =~ ^aarch64 ]] || [[ $MACHTYPE =~ ^arm64 ]] ; then
   # Arm based macs crash on a memory check unless this is added
   VERTICA_ENV+=(-e VERTICA_MEMDEBUG=2)
 fi
-docker-compose exec ${VERTICA_ENV[@]} vertica /opt/vertica/bin/admintools -t create_db --database=example --password= --hosts=localhost | $green || exit $?
+docker-compose -f $DOCKER_COMPOSE_YML exec ${VERTICA_ENV[@]} vertica /opt/vertica/bin/admintools -t create_db --database=example --password= --hosts=localhost | $green || exit $?
 
 # create a simple table to store messages
-docker-compose exec vertica vsql -c 'create flex table KafkaFlex()' | $green || exit $?
+docker-compose -f $DOCKER_COMPOSE_YML exec vertica vsql -c 'create flex table KafkaFlex()' | $green || exit $?
 
 # create an operator
-docker-compose exec vertica vsql -c 'create user JimmyKafka' | $green || exit $?
+docker-compose -f $DOCKER_COMPOSE_YML exec vertica vsql -c 'create user JimmyKafka' | $green || exit $?
 
 # create a resource pool
-docker-compose exec vertica vsql -c 'create resource pool Scheduler_pool plannedconcurrency 1' | $green || exit $?
+docker-compose -f $DOCKER_COMPOSE_YML exec vertica vsql -c 'create resource pool Scheduler_pool plannedconcurrency 1' | $green || exit $?
+
+# set up TLS SSL
+if [[ -z $NO_SSL ]] ; then
+ # docker-compose -f $DOCKER_COMPOSE_YML exec vertica vsql -c 'ALTER TLS CONFIGURATION server CERTIFICATE server_cert' | $green || exit $?
+:
+fi
 
 # create a couple topics
-docker-compose exec kafka kafka-run-class.sh kafka.admin.TopicCommand --create --partitions 10 --replication-factor 1 --topic KafkaTopic1 --bootstrap-server kafka:9092 | $green || exit $?
-docker-compose exec kafka kafka-run-class.sh kafka.admin.TopicCommand --create --partitions 10 --replication-factor 1 --topic KafkaTopic2 --bootstrap-server kafka:9092 | $green || exit $?
+docker-compose -f $DOCKER_COMPOSE_YML exec kafka kafka-run-class.sh kafka.admin.TopicCommand --create --partitions 10 --replication-factor 1 --topic KafkaTopic1 --bootstrap-server kafka:9092 | $green || exit $?
+docker-compose -f $DOCKER_COMPOSE_YML exec kafka kafka-run-class.sh kafka.admin.TopicCommand --create --partitions 10 --replication-factor 1 --topic KafkaTopic2 --bootstrap-server kafka:9092 | $green || exit $?
 
 fi
+
 ###################
 # SETUP SCHEDULER #
 ###################
@@ -80,12 +144,7 @@ if [[ $steps =~ setup ]]; then
 # define a couple microbatches
 # (using one "docker run" because the startup costs add up)
 docker run \
-  --rm \
-  -v $PWD/example.conf:/etc/vkconfig.conf \
-  -v $PWD/vkafka-log-config-debug.xml:/opt/vertica/packages/kafka/config/vkafka-log-config.xml \
-  -v $PWD/log:/opt/vertica/log \
-  --user $(id -u):$(id -g) \
-  --network $NETWORK \
+  "${DOCKER_OPTS[@]}" \
   vertica/kafka-scheduler:$VERTICA_VERSION bash -c "
     echo 'Creating scheduler schema' ; \
     vkconfig scheduler \
@@ -153,6 +212,7 @@ docker run \
     echo "Kafka Scheduler setup failed" | $red >&2
   fi
 fi
+
 #####################
 # RUN THE SCHEDULER #
 #####################
@@ -164,13 +224,7 @@ docker rm kafka_scheduler 2>/dev/null | $green
 # run this in the background
 # don't color the log output becasue it can mess up the formatting
 docker run \
-  --rm \
-  -v $PWD/example.conf:/etc/vkconfig.conf \
-  -v $PWD/vkafka-log-config-debug.xml:/opt/vertica/packages/kafka/config/vkafka-log-config.xml \
-  -v $PWD/log:/opt/vertica/log \
-  --network $NETWORK \
-  --user $(id -u):$(id -g) \
-  --name kafka_scheduler \
+  "${DOCKER_OPTS[@]}" \
   vertica/kafka-scheduler:$VERTICA_VERSION \
     vkconfig launch \
       --conf /etc/vkconfig.conf | $blue &
@@ -187,17 +241,17 @@ if [[ $steps =~ write ]]; then
 while true; do
 
 # write a test subject with a caffine addiction
-docker-compose exec kafka bash -c 'echo "{\"Test Subject\":\"98101\", \"Diagnosis\":\"Caffine Addiction\"}" | kafka-console-producer.sh \
+docker-compose -f $DOCKER_COMPOSE_YML exec kafka bash -c 'echo "{\"Test Subject\":\"98101\", \"Diagnosis\":\"Caffine Addiction\"}" | kafka-console-producer.sh \
   --topic KafkaTopic1 \
   --bootstrap-server localhost:9092' | grep . | $green
 
 # Make sure it's there
 # This produces an eroneous error message, so grep is used to only print messages
-docker-compose exec kafka kafka-console-consumer.sh --topic KafkaTopic1 --bootstrap-server localhost:9092 --from-beginning --timeout-ms 1000 | grep '^{' | $green
+docker-compose -f $DOCKER_COMPOSE_YML exec kafka kafka-console-consumer.sh --topic KafkaTopic1 --bootstrap-server localhost:9092 --from-beginning --timeout-ms 1000 | grep '^{' | $green
 
 # wait for it to appear in vertica
 delay=0
-while ! docker-compose exec vertica vsql -t -c "SELECT compute_flextable_keys_and_build_view('KafkaFlex'); SELECT Diagnosis FROM KafkaFlex_view WHERE \"Test Subject\" = '98101'" | grep Caffine >/dev/null 2>&1; do
+while ! docker-compose -f $DOCKER_COMPOSE_YML exec vertica vsql -t -c "SELECT compute_flextable_keys_and_build_view('KafkaFlex'); SELECT Diagnosis FROM KafkaFlex_view WHERE \"Test Subject\" = '98101'" | grep Caffine >/dev/null 2>&1; do
   if ((delay++ > 20)); then
     echo "ERROR: Should have appeared within the ~10 second frame duration." | $red >&2
     break 2
@@ -206,18 +260,18 @@ while ! docker-compose exec vertica vsql -t -c "SELECT compute_flextable_keys_an
   sleep 1;
 done
 
-docker-compose exec vertica vsql -c "SELECT * FROM KafkaFlex_view" | $green
+docker-compose -f $DOCKER_COMPOSE_YML exec vertica vsql -c "SELECT * FROM KafkaFlex_view" | $green
 
 # write a test subject with a cold feet problem
-docker-compose exec kafka bash -c 'echo "{\"Test Subject\":\"99782\", \"Diagnosis\":\"Cold Feet\"}" | kafka-console-producer.sh \
+docker-compose -f $DOCKER_COMPOSE_YML exec kafka bash -c 'echo "{\"Test Subject\":\"99782\", \"Diagnosis\":\"Cold Feet\"}" | kafka-console-producer.sh \
   --topic KafkaTopic2 \
   --bootstrap-server localhost:9092' | grep . | $green
 
 # Make sure it's there
-docker-compose exec kafka kafka-console-consumer.sh --topic KafkaTopic2 --bootstrap-server localhost:9092 --from-beginning --timeout-ms 1000 | grep '^{' | $green
+docker-compose -f $DOCKER_COMPOSE_YML exec kafka kafka-console-consumer.sh --topic KafkaTopic2 --bootstrap-server localhost:9092 --from-beginning --timeout-ms 1000 | grep '^{' | $green
 
 delay=0
-while ! docker-compose exec vertica vsql -t -c "SELECT compute_flextable_keys_and_build_view('KafkaFlex'); SELECT Diagnosis FROM KafkaFlex_view WHERE \"Test Subject\" = '99782'" | grep Cold >/dev/null 2>&1; do
+while ! docker-compose -f $DOCKER_COMPOSE_YML exec vertica vsql -t -c "SELECT compute_flextable_keys_and_build_view('KafkaFlex'); SELECT Diagnosis FROM KafkaFlex_view WHERE \"Test Subject\" = '99782'" | grep Cold >/dev/null 2>&1; do
   if ((delay++ > 20)); then
     echo "ERROR: Should have appeared within the ~10 second frame duration." | $red >&2
     break 2
@@ -229,9 +283,9 @@ done
 break
 done
 
-docker-compose exec vertica vsql -c "SELECT * FROM KafkaFlex_view" | $green
-if (( $(docker-compose exec vertica vsql -t -c "SELECT count(*) FROM KafkaFlex_rej" | head -1 | sed 's/\s//g') )); then
-  docker-compose exec vertica vsql -c "SELECT * FROM KafkaFlex_rej" | $red
+docker-compose -f $DOCKER_COMPOSE_YML exec vertica vsql -c "SELECT * FROM KafkaFlex_view" | $green
+if count=$(docker-compose -f $DOCKER_COMPOSE_YML exec vertica vsql -t -c "SELECT count(*) FROM KafkaFlex_rej" | head -1 | sed 's/\s//g' 2>&1) && [[ $count =~ ^[0-9][0-9]*$ ]] && ((count)); then
+  docker-compose -f $DOCKER_COMPOSE_YML exec vertica vsql -c "SELECT * FROM KafkaFlex_rej" | $red
 fi
 
 fi
@@ -272,6 +326,6 @@ fi
 if [[ $steps =~ clean ]]; then
 
 # docker-compose uses colors, so don't override
-#docker-compose down
-docker-compose rm -svf
+docker-compose -f $DOCKER_COMPOSE_YML down
+docker-compose -f $DOCKER_COMPOSE_YML rm -svf
 fi
